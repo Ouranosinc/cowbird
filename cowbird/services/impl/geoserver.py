12fc74a1e48b66d67a407fb144fefa60f89d66c2
f-PLT <pelletier.f@gmail.com>
2021-08-13 15:46:32 -0400

    Implement Geoserver workspace deletion

--- cowbird/services/impl/geoserver.py
+++ cowbird/services/impl/geoserver.py
@@ -46,7 +46,8 @@ class Geoserver(Service):
         self.create_datastore(user_name)
 
     def user_deleted(self, user_name):
-        raise NotImplementedError
+        LOGGER.info("Creating workspace in geoserver")
+        self.remove_workspace(user_name)
 
     def permission_created(self, permission):
         raise NotImplementedError
@@ -81,6 +82,25 @@ class Geoserver(Service):
             LOGGER.error(request.text)
         else:
             LOGGER.error("There was an error creating the workspace in Geoserver : %s", name)
+
+    def remove_workspace(self, name):
+        """
+        Removes a workspace from geoserver. Will allso remove all datastores associated with
+        the workspace.
+
+        @param name: Workspace name
+        """
+        request_url = "{}/workspaces/{}?recurse=true".format(self.api_url, name)
+        request = requests.delete(url=request_url, auth=self.auth, headers=self.headers)
+
+        request_code = request.status_code
+        if request_code == 200:
+            LOGGER.info("Geoserver workspace was successfully removed : %s", name)
+        elif request_code == 403:
+            LOGGER.error(
+                "Geoserver workspace '%s' is not empty. Make sure `recurse` is set to `true` to delete workspace")
+        elif request_code == 404:
+            LOGGER.error("Geoserver workspace '%s' was not found.", name)
 
     def create_datastore(self, workspace_name):
         # type (Geoserver, int, str) -> int
